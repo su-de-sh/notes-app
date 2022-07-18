6569f9807abbd69f712cb82b0176bbd339fe6c1e
su-de-sh <sudesh7443@gmail.com>
2022-07-18 12:01:36 +0545

    feat:uncontrolled form added

--- src/App.js
+++ src/App.js
@@ -1,7 +1,35 @@
-import React from "react";
+import { useState } from "react";
+import Note from "./components/Note";
 
-const App = () => {
-  return <div>App</div>;
+const App = (props) => {
+  const [notes, setNote] = useState(props.notes);
+
+  const addNote = (event) => {
+    console.dir(event.target.newnote.value);
+    event.preventDefault();
+    const newNote = {
+      id: Object.keys(notes).length,
+      content: event.target.newnote.value,
+      date: Date.now(),
+      important: true,
+    };
+    setNote([...notes, newNote]);
+  };
+
+  return (
+    <div>
+      <h1>Notes</h1>
+      <ul>
+        {notes.map((note) => (
+          <Note key={note.id} note={note} />
+        ))}
+      </ul>
+      <form onSubmit={addNote}>
+        <input name="newnote" type="text" />
+        <button type="submit">submit</button>
+      </form>
+    </div>
+  );
 };
 
 export default App;

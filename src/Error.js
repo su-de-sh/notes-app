fa42c824aab51636cb7c4318c282d5ad5c654c8e
su-de-sh <sudesh7443@gmail.com>
2022-07-19 13:29:41 +0545

    feat: added .catch with error component

---
+++ src/Error.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+const Error = ({ msg }) => {
+  return <div>{msg}</div>;
+};
+
+export default Error;
--- src/index.js
+++ src/index.js
@@ -1,9 +1,17 @@
 import ReactDOM from "react-dom/client";
 import App from "./App";
+import Error from "./Error";
 import axios from "axios";
 
-axios.get("http://localhost:3001/notes").then((response) => {
-  ReactDOM.createRoot(document.getElementById("root")).render(
-    <App notes={response.data} />
-  );
-});
+axios
+  .get("http://localhost:3001/notes1")
+  .then((response) => {
+    ReactDOM.createRoot(document.getElementById("root")).render(
+      <App notes={response.data} />
+    );
+  })
+  .catch((error) => {
+    ReactDOM.createRoot(document.getElementById("root")).render(
+      <Error msg={error.message} />
+    );
+  });

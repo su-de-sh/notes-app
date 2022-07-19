90178e5f6c0bf796494480adc9e62abec547101b
su-de-sh <sudesh7443@gmail.com>
2022-07-19 11:41:12 +0545

    feat: data read from json server

--- src/index.js
+++ src/index.js
@@ -1,4 +1,9 @@
 import ReactDOM from "react-dom/client";
 import App from "./App";
+import axios from "axios";
 
-ReactDOM.createRoot(document.getElementById("root")).render(<App />);
+axios.get("http://localhost:3001/notes").then((response) => {
+  ReactDOM.createRoot(document.getElementById("root")).render(
+    <App notes={response.data} />
+  );
+});
